9ad3543c95b925be86f2636b1b78f839b30183eb
Turck FT BDM Group <132084854+TurckBDMs-Main@users.noreply.github.com>
2023-08-02 12:46:01 -0400

    Update multi_state_string_generator.py

--- multi_state_string_generator.py
+++ multi_state_string_generator.py
@@ -1,18 +1,18 @@
 #script to take 'states' list from an ARGEE.st or .txt project file and outputs multi-state display string
 
 
-prefix = 'MULTI_STATE_DISPLAY_STRING("",state,"1.5","black","transparent",'
-
-suffix = ')'
-
-
 from tkinter import filedialog  
 import os
 
-path = filedialog.askopenfilename()
+#prompt user for file
+path = filedialog.askopenfilename(title = ("SELECT ARGEE .st or .txt"),
+                                  filetypes=(("ARGEE Project", "*.st"),("ARGEE Text Output", "*.txt"),("all files", "*.*")))
 foundline = False
 i=0
 outputstring = ''
+prefix = 'MULTI_STATE_DISPLAY_STRING("",state,"1.5","black","transparent",'
+suffix = ')'
+
 if path.endswith('.txt') or path.endswith('.st'):
     file = open(path,'r')
     contents = file.readlines()
@@ -30,8 +30,10 @@ if path.endswith('.txt') or path.endswith('.st'):
         elif foundline and not 'end_enum' in line:
             outputstring =  (outputstring + str(i) +',"'+line[1:].replace('_',' ').upper()+'","black","transparent",').replace('\n','')
             i+=1
-        
-            
-    print(prefix+outputstring[:-1]+suffix)
-    file.close()
+else:
+    print('invalid file type')
+
+#add prefix and suffix to output display string, slice final ',' character and print     
+print(prefix+outputstring[:-1]+suffix)
+file.close()
         
